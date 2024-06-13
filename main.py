902a72b0e3160a8b6d512abdd82097efe271a684
JuliaVodchits <julia.vodchits@ya.ru>
2024-06-13 17:57:56 +0300

    1st

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 import pygame
 
+
+class Image():
+    def __init__(self, path, speed=1):
+        self.__image = pygame.image.load(path)
+        self.speed = speed
+        self.__rect = self.__image.get_rect()
+        self.x = self.__rect.x
+        self.y = self.__rect.y
+        self.width = self.__rect.width
+        self.height = self.__rect.height
+        # print(f"{self.width} x {self.height}")
+
+    def set_x(self, new_x):
+        self.__rect.x = new_x
+
+    def set_y(self, new_y):
+        self.__rect.y = new_y
+
+    def rect(self):
+        return self.__rect
+
+    def image(self):
+        return self.__image
+
 pygame.init()
+
 window_size = (800, 600)
 screen = pygame.display.set_mode((window_size))
 pygame.display.set_caption("Тестовый проект")
-image = pygame.image.load("")
+icon = pygame.image.load("img/pacman_icon.png")
+pygame.display.set_icon(icon)
+
+img_packman = Image("img/pacman_80.png")
+img_monster1 = Image("img/monster_red_80.png")
 
 # Игровой цикл
 running = True
@@ -14,7 +43,46 @@ while running:
             running = False
             break
 
-    screen.fill((0, 0, 0))
+        # if event.type == pygame.MOUSEMOTION:
+        #     mouse_x, mouse_y = pygame.mouse.get_pos()
+        #     img_packman.__rect.center = mouse_x, mouse_y
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        x = img_packman.x - img_packman.speed
+        if x < 0:
+            x = 0
+        img_packman.set_x(x)
+
+    if keys[pygame.K_RIGHT]:
+        x = img_packman.x + img_packman.speed
+        if x > window_size[0] - img_packman.width:
+            x = window_size[0] - img_packman.width
+        img_packman._Image__rect.x = x
+        # img_packman.set_x(x)
+
+    if keys[pygame.K_UP]:
+        y = img_packman.y - img_packman.speed
+        if y < 0:
+            y = 0
+        img_packman.set_x(y)
+
+    if keys[pygame.K_DOWN]:
+        y = img_packman.y + img_packman.speed
+        if y > window_size[1] - img_packman.height:
+            y = window_size[1] - img_packman.height
+        img_packman.set_x(y)
+
+    img_monster1.set_x(window_size[0] - img_monster1.width)
+    img_monster1.set_y(window_size[1] - img_monster1.height)
+
+    # if img_packman.__rect.colliderect(img_monster1.__rect)
+
+    screen.fill((255, 255, 255))
+    screen.blit(img_packman.image(), img_packman.rect())
+    screen.blit(img_monster1.image(), img_monster1.rect())
+
     pygame.display.flip()   # обновление экрана
 
 pygame.quit()
